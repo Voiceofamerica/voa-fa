f5bb6c20f5126d6d8b35df0de2973cb39b6aba7b
Kevin Groat <apps@kevingroat.io>
2018-03-19 11:40:26 -0400

    Updating labels for circumvention and media drawer

--- src/labels.tsx
+++ src/labels.tsx
@@ -39,7 +39,7 @@ export const categorySettingsLabels = {
 export const circumventionDrawerLabels = {
   content: (
     <div>
-      This is where the circumvetion would be explained
+      اتصال شما به VOA خصوصی، امن و ناشناس است
     </div>
   ),
 }
@@ -71,24 +71,11 @@ export const liveStreamLabels = {
   notifyMe: 'Notify Me',
 }
 
-const hilight: React.CSSProperties = {
-  color: '#0162B1',
-}
-
 export const mediaPlayerLabels = {
   empty: (
     <div>
       <p>
-        这是美国之音的<span style={hilight}>多媒体</span>播放器。 当您选择一个伴随音频或视频的故事时，它在这里播放。
-      </p>
-      <p>
-        您可以随时查看原始故事或查找新故事，而无需通过在此页面上向下滑动来停止音频或视频。
-      </p>
-      <p>
-        通过从屏幕底部的圆形蓝色<span style={hilight}>多媒体</span>按钮向上滑动来再次打开此屏幕。
-      </p>
-      <p>
-        通过点击<span style={hilight}>多媒体</span>按钮启动和停止音频或视频。
+        ین پخش کننده چند رسانه ای VOA است. هنگامی که شما یک داستان را انتخاب می کنید که دارای صوتی یا تصویری همراه است، آن را در اینجا بازی می کند. شما همیشه می توانید داستان اصلی را مشاهده کنید و یا داستان های جدید را بدون متوقف کردن صدا یا ویدئو با کشیدن بر روی این صفحه، پیدا کنید. باز کردن این صفحه دوباره با کشیدن از دکمه چند رسانه ای دور در پایین صفحه. با ضربه زدن به دکمه چند رسانه ای، صوتی یا تصویری را شروع و متوقف کنید.
       </p>
     </div>
   ),
