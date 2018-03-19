4daa34b419e186394460e3883e91f468a3dea572
Kevin Groat <apps@kevingroat.io>
2018-03-19 17:59:33 -0400

    Changed truncation length

--- src/helpers/truncation.ts
+++ src/helpers/truncation.ts
@@ -1,6 +1,9 @@
+
+const MAX_LENGTH = 33
+
 export function truncateTitleText (title: string, hasIcon: boolean) {
-  if (title.length > 24 || (title.length === 24 && hasIcon)) {
-    const truncateLength = hasIcon ? 22 : 23
+  if (title.length > MAX_LENGTH || (title.length === MAX_LENGTH && hasIcon)) {
+    const truncateLength = hasIcon ? MAX_LENGTH - 2 : MAX_LENGTH - 1
     title = title.slice(0, truncateLength) + '…'
   }
   return title
