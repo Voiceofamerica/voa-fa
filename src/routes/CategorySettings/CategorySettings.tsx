b779cd41453b345c5c431f74ac372d719976da31
Matt McHugh <mchughmk@gmail.com>
2018-03-20 09:54:56 -0400

    Update main Settings screen and Category settings (#9)

--- src/routes/CategorySettings/CategorySettings.tsx
+++ src/routes/CategorySettings/CategorySettings.tsx
@@ -7,6 +7,7 @@ import { DragDropContext } from 'react-dnd'
 import TouchBackend from 'react-dnd-touch-backend'
 
 import BottomNav, { IconItem } from '@voiceofamerica/voa-shared/components/BottomNav'
+import SvgIcon from '@voiceofamerica/voa-shared/components/SvgIcon/SvgIcon'
 
 import setCategoryOrder from 'redux-store/actions/setCategoryOrder'
 import AppState from 'types/AppState'
@@ -15,9 +16,10 @@ import Category from 'types/Category'
 import * as Query from './CategorySettings.graphql'
 import { CategorySettingsQuery } from 'helpers/graphql-types'
 import Loader from 'components/Loader'
-import { categorySettingsLabels } from 'labels'
+import { categorySettingsLabels, homeLabels } from 'labels'
+import { back } from '../../svg'
 
-import { categorySettings, pillContainer, topNav } from './CategorySettings.scss'
+import { categorySettings, pill, pillOuter, pillContainer, topNav, bottomNav, icon, headlinesSubtitle, sectionHeader, sectionName, sectionSubtitle } from './CategorySettings.scss'
 
 import CategoryPill, { PillItem } from './CategoryPill'
 
@@ -97,9 +99,8 @@ class CategorySettingsBase extends React.Component<Props, LocalState> {
   renderCategory = ({ id, name, chosen, separator }: Category & { chosen: boolean, separator: boolean }, index: number) => {
     if (separator) {
       return (
-        <div key={'separator'} style={{ gridColumnStart: 1, gridColumnEnd: 3, fontWeight: 'bold', fontSize: '1.5em', marginLeft: 5, marginRight: 5 }}>
-          {categorySettingsLabels.allCategories}
-          <span style={{ float: 'right', fontSize: '0.8em', fontWeight: 'normal' }}>{categorySettingsLabels.dragAndDrop}</span>
+        <div key={'separator'} className={sectionHeader}>
+          <div className={sectionName}>{categorySettingsLabels.allCategories}</div>
         </div>
       )
     } else {
@@ -133,17 +134,24 @@ class CategorySettingsBase extends React.Component<Props, LocalState> {
         <div className={topNav}>{categorySettingsLabels.header}</div>
         <Loader data={data}>
           <div className={pillContainer}>
-            <div style={{ fontWeight: 'bold', gridColumnStart: 1, gridColumnEnd: 3, fontSize: '1.5em', marginLeft: 5, marginRight: 5 }}>
-              {categorySettingsLabels.myCategories}
+            <div className={pillOuter}>
+              <div className={pill}>{homeLabels.headlines}</div>
+            </div>
+            <div className={headlinesSubtitle}>
+              <div>{categorySettingsLabels.headlinesFirst}</div>
+            </div>
+            <div className={sectionHeader}>
+              <div className={sectionName}>{categorySettingsLabels.myCategories}</div>
+              <div className={sectionSubtitle}>{categorySettingsLabels.dragAndDrop}</div>
             </div>
             {
               allCategoriesWithSeparator.map(this.renderCategory)
             }
           </div>
         </Loader>
-        <BottomNav>
+        <BottomNav className={bottomNav}>
           <IconItem onClick={() => history.goBack()}>
-            <i className={`mdi mdi-arrow-left`} />
+            <SvgIcon src={back} className={icon} />
           </IconItem>
         </BottomNav>
       </div>
--- src/routes/Settings/Settings.tsx
+++ src/routes/Settings/Settings.tsx
@@ -8,71 +8,172 @@ import analytics, { AnalyticsProps } from 'helpers/analytics'
 import clearAll from 'redux-store/actions/clearAll'
 import { routerActions } from 'react-router-redux'
 
+import setTextSize from 'redux-store/actions/setTextSize'
+import AppState from 'types/AppState'
+
 import {
   settingsLabels,
   categorySettingsLabels,
-  mediaSettingsLabels,
   favoritesSettingsLabels,
-  notificationSettingsLabels,
   textSettingsLabels,
 } from 'labels'
 
-import { settings, panicButtonHolder, panicButton, buttons, settingsButton, buttonIcon } from './Settings.scss'
+import { settings, panicButtonHolder, panicButton, panicButtonIcon, buttons, settingsRow, settingsButton, row, aboutVoa, content, settingsItem, settingsRowHeader, settingsValuesRow, active } from './Settings.scss'
+
+const data = {
+  textSize: [
+    {
+      description: textSettingsLabels.normalSize,
+      value: 1,
+    },
+    {
+      description: textSettingsLabels.largeSize,
+      value: 1.5,
+    },
+    {
+      description: textSettingsLabels.hugeSize,
+      value: 2,
+    },
+  ],
+}
+
+interface StateProps {
+  textSize: number
+}
 
 interface DispatchProps {
   clearAll: () => void
+  setTextSize: (size: number) => void
 }
 
 type RouteProps = RouteComponentProps<void>
 
-type Props = RouteProps & AnalyticsProps & DispatchProps
+type Props = RouteProps & AnalyticsProps & StateProps & DispatchProps
 
 class SettingsRoute extends React.Component<Props> {
-  render () {
-    const { history, clearAll } = this.props
-
+  renderPanicButton () {
     return (
-      <div className={settings}>
+      <div className={row}>
         <div className={panicButtonHolder}>
           <div className={panicButton} onClick={clearAll}>
+            <i className={`mdi mdi-alert ${panicButtonIcon}`} />
             {settingsLabels.panic}
           </div>
         </div>
-        <div className={buttons}>
-          <button className={settingsButton} onClick={() => history.push(`/settings/notifications`)}>
-            {notificationSettingsLabels.header}
-            <i className={`mdi mdi-chevron-right ${buttonIcon}`} />
-          </button>
-          <button className={settingsButton} onClick={() => history.push(`/settings/favorites`)}>
-            {favoritesSettingsLabels.header}
-            <i className={`mdi mdi-chevron-right ${buttonIcon}`} />
-          </button>
-          <button className={settingsButton} onClick={() => history.push(`/settings/categories`)}>
-            {categorySettingsLabels.header}
-            <i className={`mdi mdi-chevron-right ${buttonIcon}`} />
-          </button>
-          <button className={settingsButton} onClick={() => history.push(`/settings/text`)}>
-            {textSettingsLabels.header}
-            <i className={`mdi mdi-chevron-right ${buttonIcon}`} />
-          </button>
-          <button className={settingsButton} onClick={() => history.push(`/settings/media`)}>
-            {mediaSettingsLabels.header}
-            <i className={`mdi mdi-chevron-right ${buttonIcon}`} />
-          </button>
+      </div>
+    )
+  }
+
+  renderFavoritesSettings () {
+    const { history } = this.props
+
+    return (
+      <div className={settingsRow}>
+        <button className={settingsButton} onClick={() => history.push(`/settings/favorites`)}>
+          {favoritesSettingsLabels.header}
+        </button>
+      </div>
+    )
+  }
+
+  renderCategoriesSettings () {
+    const { history } = this.props
+
+    return (
+      <div className={settingsRow}>
+        <button className={settingsButton} onClick={() => history.push(`/settings/categories`)}>
+          {categorySettingsLabels.header}
+        </button>
+      </div>
+    )
+  }
+
+  renderTextSettings () {
+    const { textSize, setTextSize } = this.props
+
+    return (
+      <div className={settingsRow}>
+        <div className={settingsRowHeader}>
+          {textSettingsLabels.header}
+        </div>
+        <div className={settingsValuesRow}>
+          {
+            data.textSize.map(size => (
+              <div
+                key={size.value}
+                className={`${settingsItem} ${size.value === textSize ? active : ''}`}
+                onClick={() => setTextSize(size.value)}
+              >{size.description}</div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+
+  renderSendToFriends () {
+    return (
+      <div className={settingsRow}>
+        <button className={settingsButton}>
+          {settingsLabels.sendToFriends}
+        </button>
+      </div>
+    )
+  }
+
+  renderSendFeedback () {
+    return (
+      <div className={settingsRow}>
+        <button className={settingsButton}>
+          {settingsLabels.sendFeedback}
+        </button>
+      </div>
+    )
+  }
+
+  renderAboutVoa () {
+    return (
+      <div className={settingsRow}>
+        <span className={aboutVoa}>
+          {settingsLabels.aboutVoa}
+        </span>
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div className={settings}>
+        <div className={content}>
+          { this.renderPanicButton() }
+          <div className={buttons}>
+            { this.renderFavoritesSettings() }
+            { this.renderCategoriesSettings() }
+            { this.renderTextSettings() }
+            { this.renderSendToFriends() }
+            { this.renderSendFeedback() }
+            { this.renderAboutVoa() }
+          </div>
         </div>
       </div>
     )
   }
 }
 
+const mapStateToProps = (state: AppState): StateProps => ({
+  textSize: state.settings.textSize,
+})
+
 const mapDispatchToProps = (dispatch: Dispatch<any>): DispatchProps => ({
   clearAll: () => {
     dispatch(clearAll())
     dispatch(routerActions.replace('/'))
   },
+  setTextSize: (textSize: number) =>
+    dispatch(setTextSize({ textSize })),
 })
 
-const withRedux = connect(null, mapDispatchToProps)
+const withRedux = connect(mapStateToProps, mapDispatchToProps)
 
 const withAnalytics = analytics<Props>({
   state: 'Settings',
