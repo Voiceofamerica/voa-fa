60ed3ae0150d264459e232c3b0fbd2c16ace7380
Kevin Groat <apps@kevingroat.io>
2018-03-27 10:30:12 -0400

    upping video program image sizes to 300px

--- src/routes/ProgramsScreen/VideoPrograms.tsx
+++ src/routes/ProgramsScreen/VideoPrograms.tsx
@@ -111,7 +111,7 @@ const withQuery = graphql<QueryProps, ProgramVideosQuery>(
             ...c,
             image: c.image && {
               ...c.image,
-              url: mapImageUrl(c.image.url, 'w100'),
+              url: mapImageUrl(c.image.url, 'w300'),
             },
           }
         })
