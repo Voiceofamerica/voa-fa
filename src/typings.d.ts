06fbdd8d15a2f3cae1020e848ae44b9f2389ccc5
Kevin Groat <apps@kevingroat.io>
2018-04-25 14:05:08 -0400

    Updated typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,8 @@
 
+import './customTypings/gtm'
+import './customTypings/localNotification'
+import './customTypings/pdf'
+import './customTypings/selector'
+import './customTypings/statusBar'
+
 import 'cordova-plugin-device'
